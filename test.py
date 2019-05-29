eccf24a5b247fdd447d61560f428907f4d0e1edc
fh408889098 <408889098@qq.com>
2019-05-30 00:12:13 +0800

    new

--- test.py
+++ test.py
@@ -4,5 +4,8 @@ import zipfile
 dir = "D:\\tmp\data\state-farm-distracted-driver-detection"
 img_zip_dir = os.path.join(dir, 'imgs.zip')
 f = zipfile.ZipFile(img_zip_dir,'r')
+print('begin')
 for file in f.namelist():
+    print(file)
     f.extract(file, "/output/img/")
+print('done')
