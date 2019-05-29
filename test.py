aa333ccfaf41361b5259e1bf857a54c95eb55d77
fh408889098 <408889098@qq.com>
2019-05-29 23:49:12 +0800

    new

---
+++ test.py
@@ -0,0 +1,8 @@
+import os
+import zipfile
+
+dir = "D:\\tmp\data\state-farm-distracted-driver-detection"
+img_zip_dir = os.path.join(dir, 'imgs.zip')
+f = zipfile.ZipFile(img_zip_dir,'r')
+for file in f.namelist():
+    f.extract(file,"temp/")
