f4b84d0a22ec7a7abe409ab19848d2aa1e44c9b9
fh408889098 <408889098@qq.com>
2019-05-30 00:10:57 +0800

    new

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ dir = "D:\\tmp\data\state-farm-distracted-driver-detection"
 img_zip_dir = os.path.join(dir, 'imgs.zip')
 f = zipfile.ZipFile(img_zip_dir,'r')
 for file in f.namelist():
-    f.extract(file,"temp/")
+    f.extract(file, "/output/img/")
